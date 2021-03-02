f386bf1bf9afae1b725e7962397f46308a10ecb7
Elliot Parker <elliotccp@protonmail.com>
2021-03-03 10:28:55 +1100

    Add task 2 content

---
+++ ReverseInput.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ReverseInput {
+    public static void main(String[] args) {
+        System.out.println("Input desired lines to reverse. Press enter with no input to stop.");
+        Stack<String> inputLines = new Stack<>();
+
+        Scanner input = new Scanner(System.in);
+        do {
+            inputLines.push(input.nextLine());
+        } while(inputLines.peek() != "");
+        inputLines.pop();
+        input.close();
+
+        System.out.println("Outputting...");
+        while (!inputLines.empty()) {
+            System.out.println(inputLines.peek());
+            inputLines.pop();
+        }
+    }
+}
